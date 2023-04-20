7dd8d5501adfc150d842406f7a1f89fdda5a1c20
Dojy <dojydominic@gmail.com>
2023-04-20 09:11:26 +0530

    provider update

--- lib/controler/control_provider.dart
+++ lib/controler/control_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import '../model/apimodel.dart';
 
 class NewsProvider extends ChangeNotifier {
